202d1e34817fab4e74dd20e2eef29c72ca73646e
humachuka <nate.alspaugh18@gmail.com>
2015-03-09 20:49:53 -0600

    initial commit

--- weather.rb
+++ weather.rb
@@ -7,10 +7,9 @@ location = gets
 client = Weatherman::Client.new
 response = client.lookup_by_location(location).condition
 
-weather_location = weather_location(location)
-
 climate = response["text"]
 temp = response["temp"] + 32
-
+temp = temp.to_s + " degrees"
+puts temp, climate
 
 
