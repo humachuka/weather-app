79c815dbee01f19859726483bb19b9b8c46241c9
humachuka <nate.alspaugh18@gmail.com>
2015-03-12 21:28:28 -0600

    changed it to do forecasts

--- weather.rb
+++ weather.rb
@@ -1,16 +1,29 @@
 require 'yahoo_weatherman'
-
-puts "Give me your location"
-
-location = gets	
-
-client = Weatherman::Client.new
-response = client.lookup_by_location(location).condition
-
-climate = response["text"]
-temp = response["temp"] + 32
-temp = temp.to_s + " degrees"
-
-puts temp, climate
-
-
+puts "Give me your location:"
+location = gets.chomp
+def get_location(location)
+  client = Weatherman::Client.new
+  client.lookup_by_location(location)
+end
+ 
+weather = get_location(location)
+ 
+today = Time.now.strftime('%w').to_i
+ 
+weather.forecasts.each do |forecast|
+ 
+day = forecast['date']
+ 
+weekday = day.strftime('%w').to_i
+ 
+if weekday == today
+   dayName = 'Today'
+elsif weekday == today + 1
+   dayName = 'Tomorrow'
+else
+   dayName = day.strftime('%A')
+end
+ 
+puts dayName + ' is going to be ' + forecast['text'].downcase + ' with a low of ' + forecast['low'].to_s + ' and a high of ' + forecast['high'].to_s
+ 
+end
